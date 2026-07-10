050c9a5543ccb6ff574dbba6b639fb07b48154d0
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-10 19:19:31 +0530

    impoertant about rendering and object

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -13,6 +13,22 @@ function MyApp(){
     )
 }
 
+// now under the hood hamre pass jo fucntion hai woh apne ander ke tags ko object me badlata hai and then usse reder karta hai page pe toh ham tags ko banayenge object se
+
+const ReactElement = {
+    type:'a',
+    props: {
+        herf : 'https://www.google.com',
+        target: '_blank'
+    },
+    children: "Click me to go to google"
+
+}
+// affcource it doesnt run becsue we do not have the custom render ro render this object
+
+// lets make the fucntion for 1 sec 
+const anotherElement =   <a href="https://www.google.com" target='_blank'>visit google</a>
+
 createRoot(document.getElementById('root')).render(
 
     // <App />
@@ -20,6 +36,13 @@ createRoot(document.getElementById('root')).render(
     // lets use the MyApp
     // <MyApp/>
 
-    MyApp()
+    // MyApp()
+
+    // lets use the ReactElement jo ki ek obejct hai 
+    // ReactElement
+
+    // now we can use the anotherElement kyuki usme tag sahi se rendered hai 
+    anotherElement
+
 
 )
