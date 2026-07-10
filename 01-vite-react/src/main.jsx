b86b5e158fc38aa4ac6a94876943990058c34e21
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-10 18:51:22 +0530

    manipulation in main by function in that file

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -1,10 +1,25 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
+// import App from './App.jsx'
+
+// eventually ham jo app ko laa rhae hai woh usme bhi toh ek fucntion hi hai then why not we can make our own fucntion here and try to use it and render it 
+
+function MyApp(){
+    return(
+        <div>
+            <h1>Hello bro !</h1>
+        </div>
+    )
+}
 
 createRoot(document.getElementById('root')).render(
 
-    <App />
+    // <App />
+
+    // lets use the MyApp
+    // <MyApp/>
+
+    MyApp()
 
 )
