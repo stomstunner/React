021665bb225932fdb6f454b40275d8be5ee487c4
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-10 16:21:24 +0530

    first commit

---
+++ o1-basic-react-app/src/App.js
@@ -0,0 +1,13 @@
+// we have to import the components here 
+import Ujjwal from './Ujjwal'
+
+function App() {
+  return (
+    <>
+    <Ujjwal/>
+    <h1>Hello React</h1>
+    </>
+  );
+}
+
+export default App;
