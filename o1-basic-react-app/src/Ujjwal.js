d58337bac26966ba4b3d7ea2aa758d64e568038c
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-10 16:21:05 +0530

    first commit

---
+++ o1-basic-react-app/src/Ujjwal.js
@@ -0,0 +1,8 @@
+function Ujjwal(){
+    return(
+        <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis atque maxime ullam consequatur, reprehenderit eum sed, soluta nostrum ipsam ab aperiam ipsa enim aliquid animi velit doloremque praesentium quas nobis.</h2>
+    )
+}
+
+// now we have to expor the components  
+export default Ujjwal
