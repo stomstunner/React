be0117ddf171ead8f560a6ecf96d792567c566bb
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-10 16:20:04 +0530

    first commit

---
+++ 01-vite-react/src/App.jsx
@@ -0,0 +1,12 @@
+import Ujjwal from "./ujjwal.jsx";
+function App() {
+  return (
+    <>
+      <Ujjwal />
+      <h4>hello react vite</h4>
+      <h4>hello react vite</h4>
+    </>
+  );
+}
+
+export default App;
