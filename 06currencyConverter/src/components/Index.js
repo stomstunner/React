ad7898aedc64d6a250f6ad21b4cdaa0dfe64080f
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-13 19:40:34 +0530

    first commit

---
+++ 06currencyConverter/src/components/Index.js
@@ -0,0 +1,6 @@
+import InputBox from './InputBox.jsx'
+
+// iss file se ham bass export karte hai sare components ko kyuki baad me hamre pass baht sare component ban jate hai 
+
+export {InputBox}
+// comma deke ham baaki componets ko bhi export karsakte hai 
