bcc95a45d352fa86d67b8e6826a195d8b5757580
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-15 11:01:27 +0530

    first commit

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+// eventually ham ek provider bayeenge like usercontext ko ham provider banayenge ki uske ander jo bhi components hoga usko access kar sakte hai hai
+
+// context api kuch nahi hai bass kind of gloabal chix hai jisse ham access kar sakte hai 
+
+// userContexProvider hamra jsx naam ka hoga jisse ham components jaissa likenge 
