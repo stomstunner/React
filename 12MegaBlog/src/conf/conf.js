64cf443e9a2d8261698d6cdfed3316e2effb8f82
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-16 20:58:44 +0530

    commit for appwrite

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+}
+// ham ye object ke ander sabhi jo secret key hai uska access lenge aur uska ek key vlaue pair store karnge  but make sure ki ham usko String me store rakhe 
+
+export  default conf
