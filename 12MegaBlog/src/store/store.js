0f21082f001029eb0daf16771687272f52b00bd0
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-17 19:03:10 +0530

    store file for redux

---
+++ 12MegaBlog/src/store/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit'
+// store me sabse pahle ham configstore ko laao toolkit se 
+// then mujhe batao ki mere pass kitna reducders hai uska knowledgd do then 
+//store bana ke export kar do 
+
+export const store = configureStore({
+    reducer : {}
+})
