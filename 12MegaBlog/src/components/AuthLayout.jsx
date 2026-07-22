a1b3e87982f31f330d289c2f236ebfae6ad8741a
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-22 18:39:02 +0530

    creating a postForm

--- 12MegaBlog/src/components/AuthLayout.jsx
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -16,7 +16,7 @@ export default function Protected({children, authentication = true}) {
         else if(!authentication && authStatus !== authentication){
             navigate("/")
         }
-        setLoader(false)
+        setLoader(false)   
     }, [authStatus, navigate, authentication])
 
   return loader ? <h1>Loading...</h1> : <>{children}</>
