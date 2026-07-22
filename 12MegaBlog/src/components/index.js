92756435313cff64f5ab98acf9ef5e405abcfacb
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-22 18:36:44 +0530

    creating a postForm

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
