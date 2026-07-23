a7e23f42f7bc272b4e023442b44d904bd7040b80
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-23 16:50:26 +0530

    add router to it

--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -4,11 +4,84 @@ import { Provider } from 'react-redux'
 
 import App from './App'
 import {store} from './store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AuthLayout, Login } from './components/index.js'
+import AllPosts from './pages/AllPosts.jsx'
+import EditPost from './pages/EditPost.jsx'
+import AddPost from './pages/AddPost.jsx'
+import Home from './pages/Home.jsx'
+import Signup from './pages/Signup.jsx'
+import Post from './pages/Post.jsx'
+
+
+// now we make the router
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children: [
+      {
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path:"/login",
+        element: (
+          <AuthLayout authentication = { false} >
+            <Login />
+          </AuthLayout>
+
+        )
+      },
+      {
+        path:"/signup",
+        element:(
+          <AuthLayout authentication = {false}>
+            <Signup/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:"/all-posts",
+        element:(
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:"/add-post",
+        element:(
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        )
+      },
+      {
+        path:"/edit-post/:slug",
+        element:(
+          <AuthLayout authentication>
+            {" "}
+            <EditPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/post/:slug',
+        element:<Post/>,
+      },
+
+
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
  
     <Provider store={store}>
-      <App />
+      <RouterProvider  router={router} />
     </Provider>
   
 )
