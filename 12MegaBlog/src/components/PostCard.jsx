2aa676eac8c775818047351734f2a16a8b176b5a
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-23 18:45:47 +0530

    debugging

--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 // lets import the appwriteservice kyuki hame id chaiye title chaiye aur featured image bhi chaiye 
-import appwriteService from '../appwrite/config'
+import appwriteservice from '../appwrite/config'
 
 // we need the id title and featured image from the appwrite 
 // id ka syntax aissa hi hai appwrite me 
@@ -17,7 +17,7 @@ function PostCard({$id,title, featuredImage}) {
                 {/* yaha pe hamare pass ye iamge hote hai jo ki ham getFilePreview jo property banaye the appwriteme woh se leeayenge aur uss file preview ko call kanre ke liye appwrite hamse ek file id mangata hai toh ham yaha jab uss getfile preview ko call aknrege toh featured image ko pass kar denge kyuki featured image hi hamare pass ek iamge od deta hai 
                 // jo getfilepreview hai woh ham fileka url bhejta hai jisse ki ham image ke source me daal denge 
                 // featurediamge me hamra iamge nahi hoga balki iamge ka id hota hai jo hame save kate time milta hai  */}
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
+                <img src={appwriteservice.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
 
             </div>
             <h2
