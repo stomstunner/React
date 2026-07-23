0f295a66d4b91430d9785156c65ed2f89a98f7d0
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-23 18:47:44 +0530

    debugging

--- 12MegaBlog/src/store/store.js
+++ 12MegaBlog/src/store/store.js
@@ -1,8 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
+import authSlice from './authSlice';
 // store me sabse pahle ham configstore ko laao toolkit se 
 // then mujhe batao ki mere pass kitna reducders hai uska knowledgd do then 
 //store bana ke export kar do 
 
 export const store = configureStore({
-    reducer : {     }
+    reducer : {  
+        auth : authSlice,   
+        //TODO: add more slices here for posts
+    }
 })
